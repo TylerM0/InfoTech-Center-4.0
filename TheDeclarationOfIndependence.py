32d90ae5baea7c041f313cf8269a4414866b76af
Tyler Mattson <02tyler.mattson@gulllakecs.org>
2023-01-24 14:07:06 -0500

    Add ... to my loading page

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -9,9 +9,26 @@ drivers know that the InfoTech Center 4.0 OS is loading
 
 #Import Libraries Here
 import time
+import sys  # I imported the system library for further use in code.
 
+print('\n\033[1;34;40m Welcome to the InfoTech Center')
+
+x = 0
+a = 0
+
+time.sleep(2)
+print('')
+
+while x != 20:
+    x += 1
+    b = ("\033[3;33;40m Infotech Center 4.0 OS is Loading" + "." * a)
+    a = a + 1
+    sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
+    time.sleep(0.5)
+    if a == 4:
+        a = 0
+    if x == 20:
+        print('\033[1;32;40m Done!')
 timeToSleep = 2
 
-print("/n/nWelcome - InfotechCenter 4.0")
-time.sleep(timeToSleep)
-print("/nInfotech Center 4.0 OS is loading")
+
