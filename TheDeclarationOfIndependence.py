fce5f71b0ed9a72ecde56c3521c3601dfebb5f1f
Tyler Mattson <02tyler.mattson@gulllakecs.org>
2023-01-30 13:28:01 -0500

    Update... Colors - Text design

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -9,9 +9,9 @@ drivers know that the InfoTech Center 4.0 OS is loading
 
 #Import Libraries Here
 import time
-import sys  # I imported the system library for further use in code.
+import sys
 
-print('\n\033[1;34;40m Welcome to the InfoTech Center')
+print('\n\033[3;30;36m Welcome - InfoTechCenter 4.0')
 
 x = 0
 a = 0
@@ -21,14 +21,13 @@ print('')
 
 while x != 20:
     x += 1
-    b = ("\033[3;33;40m Infotech Center 4.0 OS is Loading" + "." * a)
+    b = ("\033[3;30;36m Infotech Center 4.0 OS is Loading" + "." * a)
     a = a + 1
-    sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
+    sys.stdout.write('\r'+b)
     time.sleep(0.5)
     if a == 4:
         a = 0
     if x == 20:
-        print('\033[1;32;40m Done!')
+        print('\n\n\033[3;31;36m Mission Accomplished - Retina Scanned - Access Granted!')
 timeToSleep = 2
-
 
