0840f54769c1d1f2dfecbcca61f56f385e2ee976
Tyler Mattson <02tyler.mattson@gulllakecs.org>
2023-02-09 13:52:03 -0500

    Defined Weather Function and called - Stable

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,4 +1,21 @@
 # Programmer: Tyler Mattson
-# Date: 1.20.2023
-# Program: Infotech Center Upgrades
+# Date: 2.8.2023
+# Program: Weather System Updates
 
+#Import Libraries here
+import random
+
+#Create weather conditions in a list and choose it randomly
+def weather():
+	weatherForcast = ["Snowing", "Blizzard", "Raining", "Foggy", "Windy", "Icy", "Sunshine"]
+	weatherCondition = random.choice(weatherForcast)
+	return weatherCondition
+
+# Variable to call weather() once in our VRS()
+weatherAlert = weather()
+
+print(weatherAlert)
+
+# VRS() to respond to the weather condition
+def vehicleResponseSystem():
+	print("Howdy")
